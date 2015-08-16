b7b599bbf7fbedee81001f411b8e052b2e389a94
Joseph211219479 <jd.josephdavids@gmail.com>
2015-08-16 09:25:48 +0200

    Date needs formatting

--- app/src/common/controllers/main.js
+++ app/src/common/controllers/main.js
@@ -1,10 +1,24 @@
 'use strict';
 
-app.controller('MainCtrl',['$scope','funds', function($scope, funds){
+//data called not formatted ... create service withe formatted data
+app.controller('MainCtrl',['$scope','funds' , function($scope, funds ){
   funds.success(function(data){
     $scope.fundList = data;
+  })
+}])
+
+//one controller per html
+/*.controller('DateSplit',['ldate',function(ldate){
+  ldate.success(function(data){
+    $scope.sDate = data;
+    $scope.splitter = function(ld){
+     var dt = ld.split('/');
+     var nDate =new Date( dt[2] +',' +dt[1]+'+' +dt[0]);
+     return nDate;
+     };
   });
-}]);
+}])*/
+
 
 /*
 angular.module('fundInformationApp')
--- app/src/common/services/fundService.js
+++ app/src/common/services/fundService.js
@@ -4,10 +4,38 @@ app.factory('funds', ['$http',
   function($http){
     return $http.get('src/common/jsonDataFiles/fund.json').
       success(function(data){
+        //$scope.scopeLdate = internalData(data,'fund' )
         return data;
       })
       .error(function(err){
         return err;
       });
-  }]);
+  }]).
+  filter('ldate', function(){
+    return function(input) {
+      var dte = input.split('/');
+      var sDate = new Date(dte[2] + ',' + dte[1] + ',' + dte[0]);
+      return sDate;
+    }
+});
+
+/*
+app.factory('format', function($scope ,rawData){
+  var toProcess = {};
+  var dataArray =[];
+  rawData.success(function(){
+    toProcess = $scope.fundList.getByName('fund');
+    angular.forEach(toProcess, function(value,key){
+      if(value == '')
+        this.push(key +" : -- "  );
+      else
+        this.push(key+ " : " + value);
+    })
+    $scope.arrayList = dataArray;
+
+  });
+});
+
+*/
+
 
